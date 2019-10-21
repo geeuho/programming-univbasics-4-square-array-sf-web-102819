fd65f0bc2a5031c26cde830aaa2da5415e916a5a
Allen Shin <geeuho@gmail.com>
2019-10-21 23:40:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do
-    array[counter] = array[counter] ** 2
-    counter += 1
+    new_number = array[counter] ** 2
+    new_array.push(new_number)
+  end
+  return new_array
 end
